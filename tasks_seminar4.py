01decd68dcb6a244b4978745813f8595e9eb1730
Sophia <artsophia1621@gmail.com>
2023-09-21 22:42:16 +0400

    Add tasks

---
+++ tasks_seminar4.py
@@ -0,0 +1,25 @@
+# Task 22
+n = int(input("Введите кол-во элементов первого множества: "))
+m = int(input("Введите кол-во элементов второго множества: "))
+
+list_1 = [int(input("Введите элемент первого множества: ")) for i in range(n)]
+list_2 = [int(input("Введите элемент второго множества: ")) for i in range(m)]
+
+print(sorted(set(list_1).intersection(set(list_2))))
+
+# Task 24
+n = int(input("Введите кол-во кустов: "))
+list_1 = [int(input("Введите кол-во ягод на кусте: ")) for i in range(n)] * 2
+
+value = 0
+max_value = 0
+
+for i in range(1, len(list_1) - 1):
+    value = (list_1[i - 1] + list_1[i] + list_1[i + 1])
+
+    if value > max_value:
+        max_value = value
+
+    i += 1
+
+print(max_value)
